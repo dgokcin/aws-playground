8a6bdd85f138c3806156f5398f6db73b531b4434
Deniz Gökçin <dgokcin@gmail.com>
2019-12-08 14:33:55 +0300

    working for two davsans

--- HelloVTK/main.js
+++ HelloVTK/main.js
@@ -10,6 +10,48 @@ mapper.setInputConnection(stlReader.getOutputPort())
 var stlButtonWrapper = document.querySelector(".stlButtonWrapper");
 var stlFileInput = stlButtonWrapper.querySelector('input')
 
+function updateMulti(files) {
+    const  vtkRenderWindow = vtk.Rendering.Core.vtkRenderWindow.newInstance();
+    const vtkRenderer = vtk.Rendering.Core.vtkRenderer.newInstance();
+    const openGLRenderWindow = vtk.Rendering.OpenGL.vtkRenderWindow.newInstance();
+    
+    vtkRenderWindow.addRenderer(vtkRenderer)
+    
+    for (let i=0; i < files.length; i+=1) {
+        const stlReader = vtk.IO.Geometry.vtkSTLReader.newInstance();
+        const mapper = vtk.Rendering.Core.vtkMapper.newInstance();
+        const actor = vtk.Rendering.Core.vtkActor.newInstance(); 
+        actor.setMapper(mapper)
+        mapper.setInputConnection(stlReader.getOutputPort())
+
+        const fileReader = new FileReader();
+        fileReader.onload = function onLoad() {
+            stlReader.parseAsArrayBuffer(fileReader.result);
+            vtkRenderer.addActor(actor)
+            vtkRenderer.resetCamera();
+        };
+
+        fileReader.readAsArrayBuffer(files[i]);        
+    }
+
+    
+
+    vtkRenderWindow.addView(openGLRenderWindow);
+    var stlViewElement = document.querySelector(".stlView");
+    openGLRenderWindow.setContainer(stlViewElement);
+
+    const { width, height } = stlViewElement.getBoundingClientRect();
+    openGLRenderWindow.setSize(width, height);
+
+    const  vtkRenderWindowInteractor = vtk.Rendering.Core.vtkRenderWindowInteractor.newInstance();
+    vtkRenderWindowInteractor.setView(openGLRenderWindow);
+    vtkRenderWindowInteractor.initialize();
+    vtkRenderWindowInteractor.bindEvents(stlViewElement);
+
+    const vtkInteractorStyleTrackballCamera = vtk.Interaction.Style.vtkInteractorStyleTrackballCamera.newInstance()
+    vtkRenderWindowInteractor.setInteractorStyle(vtkInteractorStyleTrackballCamera);
+}
+
 function update() {
     const vtkRenderWindow = vtk.Rendering.Core.vtkRenderWindow.newInstance();
     const vtkRenderer = vtk.Rendering.Core.vtkRenderer.newInstance();
@@ -41,6 +83,7 @@ function handleFile(event) {
     event.preventDefault();
     const dataTransfer = event.dataTransfer;
     const files = event.target.files || dataTransfer.files;
+
     if (files.length === 1) {
         // Dosya jsde boyle okunur
         const fileReader = new FileReader();
@@ -51,6 +94,8 @@ function handleFile(event) {
         };
 
         fileReader.readAsArrayBuffer(files[0]);
+    } else if (files.length > 1) {
+        updateMulti(files)
     }
 }
 
